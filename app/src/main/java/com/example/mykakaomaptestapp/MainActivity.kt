9e9e4deffd035b1c240574700e8af5cce36aaafa
sonms <smsms5676@naver.com>
2024-02-27 21:22:28 +0900

    feature/지도 생성 및 연결 그리고 화면이동 테스트준비

--- app/src/main/java/com/example/mykakaomaptestapp/MainActivity.kt
+++ app/src/main/java/com/example/mykakaomaptestapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.mykakaomaptestapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.mykakaomaptestapp.databinding.ActivityMainBinding
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG_HOME = "home_fragment"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //getAppKeyHash()
+
+        setFragment(TAG_HOME, BlankFragment())
+
     }
+
+    /*fun getAppKeyHash() {
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for(i in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(i.toByteArray())
+
+                val something = String(Base64.encode(md.digest(), 0)!!)
+                Log.e("Debug key", something)
+            }
+        } catch(e: Exception) {
+            Log.e("Not found", e.toString())
+        }
+    }*/
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val bt = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null) {
+            bt.add(R.id.fragment_content, fragment, tag).addToBackStack(null)
+        }
+
+        val home = manager.findFragmentByTag(TAG_HOME)
+
+        if (home != null) {
+            bt.hide(home)
+        }
+
+        if (tag == TAG_HOME) {
+            if (home != null) {
+                bt.show(home)
+            }
+        }
+
+        bt.commitAllowingStateLoss()
+    }
+
 }
