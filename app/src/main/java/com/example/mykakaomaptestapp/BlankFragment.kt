c8177384627364a37ea38adaaa1cb74e79b8c383
sonms <smsms5676@naver.com>
2024-02-29 21:06:02 +0900

    feature/kakao 검색 api로 검색결과 도출

--- app/src/main/java/com/example/mykakaomaptestapp/BlankFragment.kt
+++ app/src/main/java/com/example/mykakaomaptestapp/BlankFragment.kt
@@ -2,12 +2,19 @@ package com.example.mykakaomaptestapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mykakaomaptestapp.data.ResultSearchKeyword
 import com.example.mykakaomaptestapp.databinding.FragmentBlankBinding
 import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +32,7 @@ class BlankFragment : Fragment() {
     private var param2: String? = null
 
     private lateinit var binding : FragmentBlankBinding
+    private var map : MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,28 +52,84 @@ class BlankFragment : Fragment() {
         initMap()
 
         binding.testTv.setOnClickListener {
+            searchKeyword("은행")
             val intent = Intent(requireActivity(), TestActivity::class.java)
             startActivity(intent)
         }
 
+        searchKeyword("은행")
         return binding.root
     }
 
     private fun initMap() {
-        val map = MapView(requireActivity())
+        Log.d("BlankFragment", "지도 세팅")
+        map = MapView(requireActivity())
         binding.mapMvMapcontainer.addView(map)
     }
 
+    // 키워드 검색 함수
+    private fun searchKeyword(keyword: String) {
+        val retrofit = Retrofit.Builder()   // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword)   // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(
+                call: Call<ResultSearchKeyword>,
+                response: Response<ResultSearchKeyword>
+            ) {
+                // 통신 성공 (검색 결과는 response.body()에 담겨있음)
+                Log.d("Test", "Raw: ${response.raw()}")
+                Log.d("Test", "Body: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("MainActivity", "통신 실패: ${t.message}")
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Blank", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (map != null) {
+            binding.mapMvMapcontainer.removeAllViews()
+            binding.mapMvMapcontainer.removeAllViewsInLayout()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (map != null) {
+            binding.mapMvMapcontainer.removeAllViews()
+            binding.mapMvMapcontainer.removeAllViewsInLayout()
+        } else {
+            initMap()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (map != null) {
+            binding.mapMvMapcontainer.removeAllViews()
+            binding.mapMvMapcontainer.removeAllViewsInLayout()
+        } else {
+            initMap()
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "KakaoAK 176b61538805434e8f2bec528b6b44cf"  // REST API 키
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             BlankFragment().apply {
--- app/src/main/java/com/example/mykakaomaptestapp/TestActivity.kt
+++ app/src/main/java/com/example/mykakaomaptestapp/TestActivity.kt
@@ -2,17 +2,38 @@ package com.example.mykakaomaptestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.mykakaomaptestapp.databinding.ActivityTestBinding
+import net.daum.mf.map.api.MapView
 
 class TestActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityTestBinding
+    private var map : MapView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initMap()
 
+    }
+
+    private fun initMap() {
+        Log.d("TestActivity", "지도 세팅")
+        map = MapView(this)
+        binding.mapMvMapcontainer.addView(map)
+    }
+
+
+
+    override fun onStop() {
+        super.onStop()
+        if (map != null) {
+            binding.mapMvMapcontainer.removeAllViews()
+            binding.mapMvMapcontainer.removeAllViewsInLayout()
+        }
     }
 }
